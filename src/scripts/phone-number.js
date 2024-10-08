1c2b549c038b20e9710cf057c7c5ba7cbb335401
Matheus Arakaki <arakakimath@gmail.com>
2024-10-08 11:42:51 -0400

    final template

--- src/scripts/phone-number.js
+++ src/scripts/phone-number.js
@@ -1,6 +1,7 @@
 const phoneInput = document.getElementById("clientPhone");
 let phoneNumber = "";
 const listOfNumbers = " 0123456789";
+const listOfCaracteres = "+()-"
 
 phoneInput.addEventListener("focus",() => {
   if (!phoneInput.value.includes("+"))
@@ -11,8 +12,17 @@ phoneInput.addEventListener("input", () => {
   if (!phoneInput.value.includes("+"))
     phoneInput.value = "+";
 
-  if (!listOfNumbers.includes(phoneInput.value[phoneInput.value.length - 1]))
+  if (!listOfNumbers.includes(phoneInput.value[phoneInput.value.length - 1]) && Verify()) 
     phoneInput.value = phoneInput.value.slice(0,-1)
+
+  function Verify() {
+    let contain = false
+    phoneInput.value.slice(0,-1).map((c) => {
+      if (listOfCaracteres.includes(c))
+        contain = true
+    })
+    return contain
+  }
 
   if (phoneInput.value[phoneInput.value.length - 1] === " ") {
     if (!phoneInput.value.includes("("))
